83947c0518f8c9b4868b2a8bf7781df9bac503a8
midhilesh <vtu24954@veltech.edu.in>
2026-02-20 10:02:07 +0530

    Time: 6 ms (21.55%), Space: 47.1 MB (57.65%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public int[] asteroidCollision(int[] asteroids) {
+
+        java.util.Stack<Integer> stack = new java.util.Stack<>();
+
+        for (int asteroid : asteroids) {
+
+            boolean destroyed = false;
+
+            while (!stack.isEmpty() && asteroid < 0 && stack.peek() > 0) {
+
+                if (stack.peek() < -asteroid) {
+                    stack.pop();
+                }
+                else if (stack.peek() == -asteroid) {
+                    stack.pop();
+                    destroyed = true;
+                    break;
+                }
+                else {
+                    destroyed = true;
+                    break;
+                }
+            }
+
+            if (!destroyed) {
+                stack.push(asteroid);
+            }
+        }
+
+        int[] result = new int[stack.size()];
+
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+}
